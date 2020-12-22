cbca30e08d42f7d92866c9d617c608828eaeb9a7
bilgecakar <bilgecakar123@outlook.com>
2020-12-22 19:02:19 +0300

    ListingDetailsScreen oluşturuldu ön hazırlık yapıldı

--- App.js
+++ App.js
@@ -1,7 +1,14 @@
 import React from "react";
-import { StyleSheet, View, Image, Text } from "react-native";
+import { StyleSheet, View, Image, Text, Button } from "react-native";
+
 import ViewImageScreen from "./app/Screens/ViewImageScreen";
 import WelcomeScreen from "./app/Screens/WelcomeScreen";
+import AppText from "./app/components/AppText";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import AppButton from "./app/components/AppButton";
+import colors from "./app/config/colors";
+import Card from "./app/components/Card";
+import ListingDetailsScreen from "./app/Screens/ListingDetailsScreen";
 
 export default function App() {
   return <ViewImageScreen />;
