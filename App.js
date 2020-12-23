b8b45c599f30774d41c1907cd693877cd93e66ad
AyoVayo <alikeremocal@gmail.com>
2020-12-23 17:22:51 +0300

    Accounts, Listings ve Messages sayfaları oluşturuldu.

--- App.js
+++ App.js
@@ -9,7 +9,13 @@ import AppButton from "./app/components/AppButton";
 import colors from "./app/config/colors";
 import Card from "./app/components/Card";
 import ListingDetailsScreen from "./app/Screens/ListingDetailsScreen";
+import MessagesScreen from "./app/Screens/MessagesScreen";
+import Screen from "./app/components/Screen";
+import AppIcon from "./app/components/AppIcon";
+import ListItem from "./app/components/ListItem";
+import AccountScreen from "./app/Screens/AccountScreen";
+import ListingsScreen from "./app/Screens/ListingsScreen";
 
 export default function App() {
-  return <ViewImageScreen />;
+  return <ListingsScreen />;
 }
---
+++ app/Screens/AccountScreen.js
@@ -0,0 +1,72 @@
+import React from "react";
+import { StyleSheet, View, FlatList } from "react-native";
+import AppIcon from "../components/AppIcon";
+
+import ListItem from "../components/ListItem";
+import ListItemSeperator from "../components/ListItemSeperator";
+import Screen from "../components/Screen";
+import colors from "../config/colors";
+
+const menuItems = [
+  {
+    title: "My Listings",
+    icon: {
+      name: "format-list-bulleted",
+      backgroundColor: colors.primary,
+    },
+  },
+  {
+    title: "My Messages",
+    icon: {
+      name: "email",
+      backgroundColor: colors.secondary,
+    },
+  },
+];
+
+function AccountScreen(props) {
+  return (
+    <Screen style={styles.screen}>
+      <View style={styles.container}>
+        <ListItem
+          title="Ali Kerem ÖCAL"
+          subTitle="alikeremocal@gmail.com"
+          image={require("../assets/mosh.jpg")}
+        />
+      </View>
+      <View style={styles.container}>
+        <FlatList
+          data={menuItems}
+          keyExtractor={(menuItem) => menuItem.title}
+          ItemSeparatorComponent={ListItemSeperator}
+          renderItem={({ item }) => (
+            <ListItem
+              title={item.title}
+              IconComponent={
+                <AppIcon
+                  name={item.icon.name}
+                  backgroundColor={item.icon.backgroundColor}
+                />
+              }
+            />
+          )}
+        />
+      </View>
+      <ListItem
+        title={"Log Out"}
+        IconComponent={<AppIcon name="logout" backgroundColor="#ffe66d" />}
+      />
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginVertical: 20,
+  },
+  screen: {
+    backgroundColor: colors.light,
+  },
+});
+
+export default AccountScreen;
